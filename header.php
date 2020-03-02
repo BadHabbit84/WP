a36461a11f1dd7e8011f20620e836ccd34651b9b
badhabbit84 <xbadhabbit84x@gmail.com>
2020-03-02 20:50:31 +0000

    Update header.php

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+	<?php wp_head(); ?> 
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
